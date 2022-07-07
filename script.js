fe4e0520455c51e7b27bd10b15d0948c2bd09767
Egor <hazinega@gmail.com>
2022-07-07 15:57:23 +0300

    Закончил задание №2

--- script.js
+++ script.js
@@ -46,12 +46,47 @@ let numberOfFilms = +prompt('Сколько фильмов вы уже посм�
         privat: false
     };
 
-for (let i = 0; i < 2; i++) {
+/* for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
           b = prompt('На сколько оцените его?', '');
-    personalMovieDB.movies[a] = b;
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 10) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    
+} */
+
+let i = 0;
+while (i < 2) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+          i++;
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 10) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
 }
 
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDB.count);
 console.log(personalMovieDB.movies);
 
 
